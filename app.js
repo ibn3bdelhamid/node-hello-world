5c4b1aefc80bb5286f2b50123c3095d141970eee
Sheikh Asif <sh.asifhamid@outlook.com>
2026-01-28 11:10:29 +0300

    changes

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send(
-    "Hello World from Jenkins CI/CD , this is a change after webhook test wotj ngrok🚀",
-  );
+  res.send("Update welcome message🚀");
 });
 
 app.listen(3000, () => {
